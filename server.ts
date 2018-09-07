cf8b42737e2eca9bfa0a8117cebb57995cb002f0
freewind <nowindlee@gmail.com>
2018-09-07 20:48:05 +0800

    improve server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import * as Hapi from 'hapi'
+import {Request, ResponseToolkit} from 'hapi'
 
-// Create a server with a host and port
 const server = new Hapi.Server({
     host: 'localhost',
     port: 8000
@@ -9,21 +9,12 @@ const server = new Hapi.Server({
 server.route({
     method: 'GET',
     path: '/hello',
-    handler: function (request, h) {
+    handler: function (request: Request, h: ResponseToolkit) {
         return 'hello world'
     }
-})
+});
 
-// Start the server
-async function start() {
-    try {
-        let started = await server.start()
-        console.log('Server running at:', server.info.uri)
-        return started
-    } catch (err) {
-        console.log(err)
-        process.exit(1)
-    }
-}
-
-start()
+(async () => {
+    await server.start()
+    console.log('Server running at:', server.info.uri)
+})()
